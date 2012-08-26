03edcc7ac53832c05aca4b8d499e91cdb1219add
Per Sandström <per@helloper.com>
2012-08-26 16:27:10 +0200

    Set correct dirs for images and yml

--- update.php
+++ update.php
@@ -2,8 +2,8 @@
 require 'vendor/autoload.php';
 
 $source = 'https://docs.google.com/spreadsheet/pub?key=0AnhHhmlRnJsSdEtuZGZJNU43eFlxclg0aXdkTXNPZlE&output=csv';
-$yml_dir = 'jekyll/_podcast/';
-$image_dir = 'jekyll/img/podcasts/';
+$yml_dir = __DIR__ . '/jekyll/_podcast/';
+$image_dir = __DIR__ . '/jekyll/img/podcasts/';
 
 $yml_creator = new FootballPodcasts\YMLCreator($source, $yml_dir, $image_dir);
 
