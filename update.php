4ae8a6eee140139c7630d1b6a056c22a0e7618ba
Per Sandström <per@helloper.com>
2012-08-26 16:47:01 +0200

    Correct class declaration

--- update.php
+++ update.php
@@ -5,6 +5,6 @@ $source = 'https://docs.google.com/spreadsheet/pub?key=0AnhHhmlRnJsSdEtuZGZJNU43
 $yml_dir = __DIR__ . '/jekyll/_podcast/';
 $image_dir = __DIR__ . '/jekyll/img/podcasts/';
 
-$yml_creator = new FootballPodcasts\YMLCreator($source, $yml_dir, $image_dir);
+$yml_creator = new \FootballPodcasts\YMLCreator($source, $yml_dir, $image_dir);
 
 $yml_creator->readSource();
