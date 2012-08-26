9c4efd49c74ec5c9f749e930a188eaf44fdfbcf5
Per Sandström <per@helloper.com>
2012-08-26 14:58:55 +0200

    Removed Jekyll _site dir

--- update.php
+++ update.php
@@ -1,11 +1,12 @@
 <?php
+require 'vendor/autoload.php';
 
-require_once 'lib/YMLCreator.php';
+//require_once 'lib/YMLCreator.php';
 
 $source = 'https://docs.google.com/spreadsheet/pub?key=0AnhHhmlRnJsSdEtuZGZJNU43eFlxclg0aXdkTXNPZlE&output=csv';
 $yml_dir = 'jekyll/_podcast/';
 $image_dir = 'jekyll/img/podcasts/';
 
-$yml_creator = new YMLCreator($source, $yml_dir, $image_dir);
+$yml_creator = new Footballpodcasts\YMLCreator($source, $yml_dir, $image_dir);
 
 $yml_creator->readSource();
