9d058b43e8b48da86f772ed7c2d154a04e4ffe28
Per Sandström <per@helloper.com>
2012-08-26 15:14:39 +0200

    Proper autoloading in update.php

--- update.php
+++ update.php
@@ -1,12 +1,10 @@
 <?php
 require 'vendor/autoload.php';
 
-//require_once 'lib/YMLCreator.php';
-
 $source = 'https://docs.google.com/spreadsheet/pub?key=0AnhHhmlRnJsSdEtuZGZJNU43eFlxclg0aXdkTXNPZlE&output=csv';
 $yml_dir = 'jekyll/_podcast/';
 $image_dir = 'jekyll/img/podcasts/';
 
-$yml_creator = new Footballpodcasts\YMLCreator($source, $yml_dir, $image_dir);
+$yml_creator = new FootballPodcasts\YMLCreator($source, $yml_dir, $image_dir);
 
 $yml_creator->readSource();
